6f475506b30b7a158499a233c5722bc272f9e55c
Mikael B. <borges.mikael@gmail.com>
2025-03-05 22:56:07 +0100

    push current work

--- server.js
+++ server.js
@@ -1,55 +1,62 @@
-require('dotenv').config()
+require("dotenv").config();
 
 // IMPORTS :
-const cors = require('cors'),
-      express = require('express'),
-      mongoose = require('mongoose')/*,
+const cors = require("cors"),
+  express = require("express"),
+  mongoose = require("mongoose"); /*,
       bodyParser = require('body-parser')*/
 
-
 // MIDDLEWARE :
-const app = express()
+const app = express();
 //parse les url
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // sert à ce que req.body soit reconnu (la propriété body)
+app.use(cors()); // Autorise toutes les front à taper l'api
 //app.use(cors({origin: '*'}))
 
+// Plus besoin maintenant de bodyParser
 /* app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json()) */
 
-//ici on recup tout le dossier pubic (css, fonts, img, js)
+//ici on recup tout le dossier public (css, fonts, img, js)
 /* app.use(express.static(__dirname + '/public'));
 //ici on gère l'affichage des templates front
 app.set('views', './views');
 app.set('view engine', 'ejs'); */
 
-
 // SESSION :
-let session = require('express-session')/* ,
+let session = require("express-session"); /* ,
     parseurl = require('parseurl'),
     store = new session.MemoryStore() */
 
-//session va gérer la création/vérification du token lors du login
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { 
-    secure: false,
-    maxAge: 1000 * 3600 * 24 * 365,
-  },
-  //store,
-}))
+//session va gérer la création/vérification du token lors du login : Pas obligé
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 3600 * 24 * 365,
+    },
+    //store,
+  })
+);
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
 
   if (!req.session.user) {
-    req.session.user = null
-    req.session.isLogged = false
+    req.session.user = null;
+    req.session.isLogged = false;
   }
 
   /*console.log('MEMORY STORE')
@@ -63,33 +70,35 @@ app.use(function (req, res, next) {
 
   //conditions pour les accés aux routes avec restrictions qui redirigent vers le login si il n'est pas connecté ou admin
 
-  next()
-})
+  next();
+});
 
-const authRoutes = require('./routes/authRoutes')
-const userRoutes = require('./routes/userRoutes')
-const annoncesRoutes = require('./routes/annoncesRoutes')
-
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const annoncesRoutes = require("./routes/annoncesRoutes");
 
 // Configuration de l'objet Promise utilisé par mongoose (ici, ce seront celles dans Node.js -> global.Promise)
 mongoose.Promise = global.Promise;
 
 // Adapter en fonction de la configuration sur le compte "Atlas"
 
-let connectionString = process.env.MONGODB_LOCAL_URL
+let connectionString = process.env.MONGODB_LOCAL_URL;
 
 // Si dans le fichier .env on change la valeur de NODE_ENV et qu'on met
 // 'prod', on se connectera à la base de données en ligne
 // autre chose que 'prod', on se connectera à la base de données en locale
-if (process.env.NODE_ENV === 'prod') {
-  connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.${process.env.MONGODB_ACCOUNT}.mongodb.net/Database?retryWrites=true&w=majority`
+if (process.env.NODE_ENV === "prod") {
+  connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.${process.env.MONGODB_ACCOUNT}.mongodb.net/Database?retryWrites=true&w=majority`;
 }
 
 // Connexion à la base mongo :
 mongoose
   //.connect(connectionString) // online
   //.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }) // offline
-  .connect(connectionString || connectionString, { useNewUrlParser: true, useUnifiedTopology: true }) // twices cases
+  .connect(connectionString || connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) // twices cases
   .then((db) => {
     // Démarrage du serveur (qui ne démarre QUE si la connexion à la base mongo est bien établie!)
     // console.log("CONNECTÉ")
@@ -102,14 +111,14 @@ mongoose
         res.json({post: "tueur", crimes: 322})
     }) */
     //appel de nos routes
-    authRoutes(app, db)
-    userRoutes(app, db)
-    annoncesRoutes(app, db)
+    authRoutes(app, db);
+    userRoutes(app, db);
+    annoncesRoutes(app, db);
 
     // Bien utiliser .PORT pour utiliser le port d'heroku
     // 3306 c'est pour le local
-    app.listen(process.env.PORT || 3306, function() {
-      console.log('api prête')
+    app.listen(process.env.PORT || 3306, function () {
+      console.log("api prête");
     });
   })
-  .catch(err => console.error(err.message));
+  .catch((err) => console.error(err.message));
